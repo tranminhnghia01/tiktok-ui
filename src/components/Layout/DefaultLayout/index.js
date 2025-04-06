348453e63b7d13d32a072ffe60420597d534f065
tranminhnghia01 <tranminhnghia.a01@gmail.com>
2025-04-06 21:21:29 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,15 +1,19 @@
+import classNames from 'classnames/bind';
 import Header from '~/components/Layout/components/Header';
-import Sidebar from '~/components/Layout/components/Sidebar';
+import Sidebar from '~/components/Layout/DefaultLayout/Sidebar';
+import styles from './DefaultLayout.module.scss';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <div className="container">
+      <div className={cx('container')}>
         <div>
           <Sidebar />
         </div>
-        <div className="content">{children}</div>
+        <div className={cx('content')}>{children}</div>
       </div>
     </div>
   );
