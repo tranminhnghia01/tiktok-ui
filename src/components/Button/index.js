81618f910069d7d41ce60b7f8fd4ecb50752318f
tranminhnghia01 <tranminhnghia.a01@gmail.com>
2025-04-14 23:29:50 +0700

    Xây dựng UI phần Header #3

---
+++ src/components/Button/index.js
@@ -0,0 +1,59 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+  to,
+  href,
+  primary = false,
+  outline = false,
+  small = false,
+  large = false,
+  text = false,
+  disabled = false,
+  rounded = false,
+  leftIcon = false,
+  rightIcon = false,
+  className,
+  children,
+  onClick,
+  ...passProps
+}) {
+  let Comp = 'button';
+  const props = { onClick, ...passProps };
+  if (to) {
+    props.to = to;
+    Comp = Link;
+  } else if (href) {
+    props.href = href;
+    Comp = 'a';
+  }
+  if (disabled) {
+    Object.keys(props).forEach((key) => {
+      if (key.startsWith('on') && typeof props[key] === 'function') {
+        delete props[key];
+      }
+    });
+  }
+  const classes = cx('wrapper', {
+    [className]: className,
+    primary,
+    outline,
+    small,
+    large,
+    text,
+    disabled,
+    rounded,
+  });
+  return (
+    <Comp className={classes} {...props}>
+      {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+      <span className={cx('title')}>{children}</span>
+      {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+    </Comp>
+  );
+}
+
+export default Button;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,29 +1,52 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import Button from '~/components/Button';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 const cx = classNames.bind(styles);
 function Header() {
+  const [searchResult, setSearchResult] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult([1, 2, 3]);
+    }, 0);
+  }, []);
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
         <div className={cx('logo')}>
           <img src={images.logo} alt="Tiktok" />
         </div>
-        <div className={cx('search')}>
-          <input placeholder="Search accounts and videos" spellCheck={false} />
-          <button className={cx('search-clear')}>
-            <FontAwesomeIcon icon={faCircleXmark} />
-          </button>
-          <FontAwesomeIcon className={cx('search-loading')} icon={faSpinner} />
+        <Tippy
+          interactive
+          visible={searchResult.length > 0}
+          render={(attrs) => (
+            <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+              <PopperWrapper> Ket qua</PopperWrapper>
+            </div>
+          )}
+        >
+          <div className={cx('search')}>
+            <input placeholder="Search accounts and videos" spellCheck={false} />
+            <button className={cx('search-clear')}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            <FontAwesomeIcon className={cx('search-loading')} icon={faSpinner} />
 
-          <button className={cx('search-btn')}>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </button>
+            <button className={cx('search-btn')}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </Tippy>
+        <div className={cx('action')}>
+          <Button text>Upload</Button>
+          <Button primary>Log In</Button>
         </div>
-        <div className={cx('action')}>Action</div>
       </div>
     </header>
   );
